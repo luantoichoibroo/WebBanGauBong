a5d20bf58c85816819b191192492c0eeb11488fc
luantoichoibroo <vluan5069@gmail.com>
2025-04-15 01:10:45 +0700

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebBanGauBong.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
